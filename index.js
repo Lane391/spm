c4961beb550c88ee1a9e55df51cdaeeba46eb6c7
goku <kakarrot.sh@qq.com>
2014-11-28 01:54:05 +0800

    引入 koa-static 和jade

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-var koa = require('koa');
-var app = koa();
+var koa = require('koa'),
+  mount = require('koa-mount'),
+ router = require('koa-router'),
+ static = require('koa-static'),
+rewrite = require('koa-rewrite'),
+   jade = require('koa-jade');
 
-app.use(function* (){
-  this.body = 'Hello World';
+var app = koa(),
+static_app = koa();
+static_app.use(static(__dirname + '/bower_components'));
+
+app.use(jade.middleware({
+  viewPath: __dirname + '/views',
+  debug: true,
+  pretty: true,
+  compileDebug: true,
+  locals: {},
+  basedir: __dirname + '/views',
+  // helperPath: [
+  //   'path/to/jade/helpers',
+  //   { random: 'path/to/lib.js' },
+  //   { _: require('lodash') }
+  // ]
+}))
+app.use(router(app));
+app.use(mount('/bower_components', static_app))
+
+
+app.get('', function *() {
+	yield this.render('index', {})
+})
+.post('/message', function *() {
+	
 });
-
 app.listen(process.env.PORT || 3000);
