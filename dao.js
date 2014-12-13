698e91834dcd04a96bc2b4460d8b427a328afa10
goku <kakarrot.sh@qq.com>
2014-12-13 17:00:34 +0800

    连接的时候发20条

--- dao.js
+++ dao.js
@@ -12,16 +12,18 @@ connection.connect(function(error) {
 
 exports.save_message = function(message, callback) {
 	QUERY_INSERT_MESSAGE = 'INSERT INTO `gkmessage` (`body`) VALUES (?);';
-	console.log(QUERY_INSERT_MESSAGE)
+	// console.log(QUERY_INSERT_MESSAGE)
 	connection.query(QUERY_INSERT_MESSAGE,
 		[JSON.stringify(message)], function(error, rows, fields) {
+			if (error)
+				console.log(error)
 			callback(rows)
 		})
 }
 
 exports.messages = function(page, callback) {
-	connection.query('SELECT * FROM `gkmessage` ORDER BY id ASC' + 
-		' LIMIT ?,10', [(page - 1) * 10],
+	connection.query('SELECT * FROM `gkmessage` ORDER BY id DESC' + 
+		' LIMIT ?,10', [(page - 1) * 20],
 
 		function(error, rows, fields) {
 			callback(rows)
--- message.js
+++ message.js
@@ -1,11 +1,23 @@
+var dao = require("./dao.js")
 
 exports.initialize = function(io) {
   io.on('connection', onConnection)
-}
+
 
 function onConnection(socket) {
   socket.join("room1")
+  dao.messages(1, function(rows) {
+  	var msg = []
+  	for (var i=0, size=rows.length; i < size; i++) {
+  		var row = rows[i];
+  		msg.push(row['body'])
+  	}
+  	io.to('room1').emit("chat", msg)
+  })
   socket.on('chat', function (message) {
+  	dao.save_message(message, function(rows) {
+  	})
     socket.broadcast.to('room1').emit("chat", message)
   })
+}
 }
