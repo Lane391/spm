0a13cad9481fc704f26d2a8f3d526eb1426b8833
goku <kakarrot.sh@qq.com>
2014-12-13 17:06:31 +0800

    fix crash

--- message.js
+++ message.js
@@ -8,7 +8,7 @@ function onConnection(socket) {
   socket.join("room1")
   dao.messages(1, function(rows) {
   	var msg = []
-  	for (var i=0, size=rows.length || 0; i < size; i++) {
+  	for (var i=0, size=rows && rows.length || 0; i < size; i++) {
   		var row = rows[i];
   		msg.push(row['body'])
   	}
