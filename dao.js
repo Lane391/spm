c788bb9a8eae264ff013606c0987f5cb65c76628
goku <kakarrot.sh@qq.com>
2014-12-14 10:30:16 +0800

    mysql 密码拼写错误

--- dao.js
+++ dao.js
@@ -4,15 +4,7 @@ var pool = mysql.createPool({
   host     : process.env.MYSQL_HOST || '127.0.0.1',
   database : process.env.MYSQL_DATABASE || 'gkmessage',
   user     : process.env.MYSQL_USERNAME || 'root',
-  password : process.env.MSYQL_PASSWORD || '',
-  port : process.env.MYSQL_port || 3306
-})
-
-console.log({
-  host     : process.env.MYSQL_HOST || '127.0.0.1',
-  database : process.env.MYSQL_DATABASE || 'gkmessage',
-  user     : process.env.MYSQL_USERNAME || 'root',
-  password : process.env.MSYQL_PASSWORD || '',
+  password : process.env.MYSQL_PASSWORD || '',
   port : process.env.MYSQL_port || 3306
 })
 
