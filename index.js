03e6ec3df2b9d709dd7bcc62442c26911f823e07
Yang Li <yaleeyang@163.com>
2015-01-17 16:26:44 +0800

    temp commit

--- index.js
+++ index.js
@@ -33,11 +33,28 @@ app.use(mount('/bower_components', static_app))
 // var server = require('http').Server(app.callback()),
 //         io = require('socket.io')(server)
 
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+}
+
+// Dummy users
+var users = [
+    new User('tj', 'tj@vision-media.ca')
+  , new User('ciaran', 'ciaranj@gmail.com')
+  , new User('aaron', 'aaron.heckmann+github@gmail.com')
+];
+
 app.get('/', function *() {
 	yield this.render('index', {})
 })
 .post('/message', function *() {
 	
+});
+
+app.get('/user', function *() {
+  console.log(this.request.query.id);
+  yield this.render('test', { users: users });
 });
 
 // require("./message").initialize(io)
